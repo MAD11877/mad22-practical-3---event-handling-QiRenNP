243980a0998dc2469a6af861203528d8f10efbe7
qiren <s10205561@connect.np.edu.sg>
2022-05-06 09:42:04 +0800

    upload project

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/group2.java
@@ -0,0 +1,25 @@
+package sg.edu.np.mad.madpractical;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class group2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_group2, container, false);
+    }
+
+    /*
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState){
+
+    }*/
+}
